daf7ab4949c43575ff2c91a4bf178aa524d72c91
Melissa <hellofucking@icloud.com>
2026-01-27 04:09:38 +0300

    Add files via upload

---
+++ app/api/users.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: application/json');
+$users = json_decode(file_get_contents('../data/users.json'), true);
+echo json_encode([
+    "status" => "success",
+    "project" => "geminy.me",
+    "total_users" => count($users),
+    "api_version" => "1.0"
+]);
